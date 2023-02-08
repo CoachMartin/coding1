05f889f96f7e8cd8e053fe0b45a069bbae0b3677
CoachMartin <122988946+CoachMartin@users.noreply.github.com>
2023-02-08 19:34:03 +0000

    var1 work

---
+++ var1.py
@@ -0,0 +1,12 @@
+###########################################################
+# File Name: var1.pay
+#
+# File Description:
+#
+# var1.py contains exersises related to the Variables,
+# Numbers, and Strings content from introtopyton.org.
+# 
+#
+# This file ....
+#
+###########################################################
