1d2062a90f5a24b3067208fac0570926496e5a19
CoachMartin <122988946+CoachMartin@users.noreply.github.com>
2024-11-05 17:31:35 +0000

    committ

--- quiz1-modifications.py
+++ quiz1-modifications.py
@@ -1,1 +1,14 @@
 
+list = ["a","b","c","d","e","f"]
+
+sum = 0
+
+for x in list:
+    if x == "a":
+        sum = sum + 1
+    elif x == "b":
+        sum = sum + 2
+    elif x == "c":
+        sum = sum + 3
+
+print(sum)
